30ff0b4cec268cc1c4daf7ebf323ebfdc9fe4a17
Jason Palmer <jpalmer@spotify.com>
2015-03-13 20:31:49 -0400

    Initial app boiler plate
    Change-Id: Icc14746abae00247314fb19b6e6fb0198323058a

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var browserify = require('browserify');
+var gulp = require('gulp');
+var transform = require('vinyl-transform');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
+var batch = require('gulp-batch');
+var connect = require('gulp-connect');
+var ghPages = require('gulp-gh-pages');
+var less = require('gulp-less');
+var path = require('path');
+var minifyCSS = require('gulp-minify-css');
+
+var getBundleName = function () {
+  var version = require('./package.json').version;
+  var name = require('./package.json').name;
+  return version + '.' + name + '.' + 'min';
+};
+
+gulp.task('connect', function () {
+  connect.server();
+});
+
+gulp.task('less', function () {
+  return gulp.src('./components/**/*.less')
+    .pipe(less())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('javascript', function () {
+  // transform regular node stream to gulp (buffered vinyl) stream 
+  var browserified = transform(function(filename) {
+    var b = browserify(filename);
+    return b.bundle();
+  });
+
+  return gulp.src('./components/app.js')
+    .pipe(browserified)
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        //.pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['**/*.js', '**/*.json', '**/*.less'], ['javascript', 'less']);
+});
+
+gulp.task('deploy', ['javascript'], function () {
+  return gulp.src(['./index.html', './dist/**/*'])
+      .pipe(ghPages({
+        force: true
+      }));
+});
+
+gulp.task('dev', ['javascript', 'less', 'connect', 'watch']);
+gulp.task('default', ['javascript', 'less']);
