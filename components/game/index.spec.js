a4cdd04739ac3958ec37a7091dd032b8c7544095
Jason Palmer <jpalmer@spotify.com>
2015-03-15 16:17:17 -0400

    Added sinon so we can spy and stub methods
    Change-Id: I535ea29a01dafe0bad6cf1a139ac389f376a42d5

--- components/game/index.spec.js
+++ components/game/index.spec.js
@@ -1,6 +1,10 @@
 var Game = require('./index');
 var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+
 var expect = chai.expect;
+chai.use(sinonChai);
 
 describe('Game', function () {
 
@@ -24,6 +28,14 @@ describe('Game', function () {
       game.initialize();
 
       expect(window.requestAnimFrame).to.exist
+    });
+
+    it('Gameloop should be called', function () {
+      sinon.spy(game, 'gameLoop');
+
+      game.initialize();
+
+      expect(game.gameLoop).to.have.been.called;
     });
   });
 });
