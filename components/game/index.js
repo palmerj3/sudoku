95a0d5ea83ad52a4420c4a7851d9b923fe2ff704
Jason Palmer <jpalmer@spotify.com>
2015-03-15 18:32:39 -0400

    Added basic cell selection logic. Will be used for future number and note entry.
    Change-Id: I85479ae178127334b084d265ac85c91d8d1119dc

--- components/game/index.js
+++ components/game/index.js
@@ -10,16 +10,56 @@
 
     this.grid = new Grid();
 
-    // Re-bind
+    this.state = {
+      mouseclick : null,
+      canvasSize : {
+        h: this.canvas.height,
+        w: this.canvas.width
+      }
+    };
+
+    // Re-bind so we can unbind and maintain function ref
     this.gameLoop = this.gameLoop.bind(this);
     this.tick = this.tick.bind(this);
     this.draw = this.draw.bind(this);
+    this.listenForUserInput = this.listenForUserInput.bind(this);
   };
 
   Game.prototype.initialize = function () {
     Util.shim.rAF.install();
 
+    this.listenForUserInput();
+    this.listenForWindowResize();
+
     this.gameLoop();
+  };
+
+  Game.prototype.listenForWindowResize = function () {
+    var self = this;
+
+    window.addEventListener('resize', function (e) {
+      self.state.canvasSize.h = window.innerWidth / 2;
+      self.state.canvasSize.w = window.innerWidth / 2;
+
+      self.canvas.width = self.state.canvasSize.w;
+      self.canvas.height = self.state.canvasSize.h;
+    });
+  };
+
+  Game.prototype.listenForUserInput = function () {
+    var self = this;
+
+    window.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      var rect = self.canvas.getBoundingClientRect();
+
+      self.state.mouseclick = {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+      };
+
+    });
   };
 
   Game.prototype.gameLoop = function () {
@@ -30,10 +70,11 @@
   };
 
   Game.prototype.tick = function () {
-    this.grid.tick();
+    this.grid.tick(this.state);
   };
 
   Game.prototype.draw = function (ctx) {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.grid.draw(ctx);
   };
 
--- components/grid/index.js
+++ components/grid/index.js
@@ -2,14 +2,34 @@
   'use strict';
 
   var Grid = function() {
-
+    this.state = {
+      highlights : {}
+    }
   };
 
-  Grid.prototype.tick = function () {
+  Grid.prototype.handleMouseClick = function (gamestate) {
+    var cellWidth = gamestate.canvasSize.w / 9,
+        cellHeight = gamestate.canvasSize.h / 9,
+        cellX = Math.floor(gamestate.mouseclick.x/cellWidth)*cellWidth,
+        cellY = Math.floor(gamestate.mouseclick.y/cellHeight)*cellHeight,
+        key = Math.floor(cellX/cellWidth) + '-' + Math.floor(cellY/cellHeight);
 
+    console.log('Canvas Dimensions (', gamestate.canvasSize.w, 'x' , gamestate.canvasSize.h, ')');
+    console.log('Cell Dimensions(', cellWidth, 'x' , cellHeight, ')');
+    console.log('Cell Coords (', cellX, ',' , cellY, ')');
+    console.log('Click Coords (', gamestate.mouseclick.x, ',' , gamestate.mouseclick.y, ')');
+    console.log('Key', key);
+
+    if (!this.state.highlights.hasOwnProperty(key)) {
+      this.state.highlights[key] = 'selected';
+    } else {
+      delete(this.state.highlights[key]);
+    }
+
+    gamestate.mouseclick = null;
   };
 
-  Grid.prototype.draw = function (ctx) {
+  Grid.prototype.drawGrid = function (ctx) {
     var width = ctx.canvas.width,
         height = ctx.canvas.height,
         lineWidth = width / 9,
@@ -48,6 +68,35 @@
 
       ctx.stroke();
     }
+  };
+
+  Grid.prototype.drawHighlights = function (ctx) {
+    var cellWidth = ctx.canvas.width / 9,
+        cellHeight = ctx.canvas.height / 9;
+
+    for (var h in this.state.highlights) {
+      if (!this.state.highlights.hasOwnProperty(h)) continue;
+
+      var h_x = Number(h.split('-')[0]),
+          h_y = Number(h.split('-')[1]),
+          cellX = h_x*cellWidth,
+          cellY = h_y*cellHeight;
+
+      //console.log('Highlight: (', cellX, ',', cellY, ')');
+
+      ctx.fillRect(cellX, cellY, cellWidth, cellHeight);
+    }
+  };
+
+  Grid.prototype.tick = function (gameState) {
+    if (gameState.mouseclick !== null) {
+      this.handleMouseClick(gameState);
+    }
+  };
+
+  Grid.prototype.draw = function (ctx) {
+    this.drawGrid(ctx);
+    this.drawHighlights(ctx);
   };
 
   module.exports = Grid;
